4789da25135f4fa6b599a9790f8353c1d4aeca82
Federico Trotta <117639221+fredowashere@users.noreply.github.com>
2022-11-24 14:41:40 +0100

    Fix styles

--- src/app/chiusure-referente/stato-utenti/stato-utenti.component.ts
+++ src/app/chiusure-referente/stato-utenti/stato-utenti.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs';
+import { AppStateService } from 'src/app/services/app-state.service';
 
 @Component({
   selector: 'app-stato-utenti',
@@ -7,13 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StatoUtentiComponent implements OnInit {
 
-  constructor() { }
+  selectedMonth$;
+
+  constructor(
+    public appState: AppStateService
+  ) { }
 
   ngOnInit(): void {
+    this.selectedMonth$ = this.appState.viewDate$
+      .pipe(
+        map(date => date.getMonth())
+      );
   }
 
-  closeDatePicker(eventData: any, dp?: any) {
+  closeDatePicker(eventDate: any, dp?: any) {
+    this.appState.viewDate = eventDate;
     dp.close();
+  }
+
+  formatDate(date: Date) {
+    if (!date) return;
+    return date.toLocaleDateString(undefined, { year: 'numeric', month: 'long' });
   }
 
 }
